3b5ec83b1b8c77c3208949efbee35ba4f8beffa9
igorcacerez <igor.cacerez@gmail.com>
2026-03-17 09:23:39 -0300

    liberando cors

--- server.js
+++ server.js
@@ -6,6 +6,21 @@ const { initializeDatabase } = require("./database/init");
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+  );
+  res.header("Access-Control-Allow-Methods", "GET,POST,PUT,PATCH,DELETE,OPTIONS");
+
+  if (req.method === "OPTIONS") {
+    return res.sendStatus(204);
+  }
+
+  return next();
+});
+
 app.use(express.json());
 
 app.get("/", (req, res) => {
