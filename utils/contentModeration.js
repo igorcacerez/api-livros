30b674208260c460e0060292ada021b0232cbfdb
igorcacerez <igor.cacerez@gmail.com>
2026-03-23 16:44:37 -0300

    bloquear palavras

--- utils/contentModeration.js
+++ utils/contentModeration.js
@@ -1,29 +1,95 @@
-const bannedTerms = new Set([
+const bannedTerms = [
   "arrombado",
+  "arrombada",
+  "ass",
+  "asshole",
+  "babaca",
+  "bastard",
+  "bastardo",
+  "besta",
+  "bitch",
+  "blowjob",
+  "boquete",
   "bosta",
+  "bucetao",
+  "bullshit",
   "buceta",
   "cacete",
   "caralho",
+  "carai",
   "corno",
+  "crap",
+  "cretino",
+  "cunt",
   "cu",
+  "damn",
   "desgraca",
+  "desgracado",
+  "desgracada",
+  "dick",
+  "dildo",
+  "douche",
+  "douchebag",
+  "escroto",
   "fdp",
+  "feladaputa",
+  "filhadaputa",
+  "filhodeputa",
+  "fuck",
+  "fucker",
+  "fucking",
   "foda",
   "foder",
+  "gozar",
+  "horny",
+  "idiota",
+  "imbecil",
+  "jackass",
+  "jerk",
+  "kct",
+  "krl",
+  "lixo",
   "merda",
+  "motherfucker",
   "otario",
+  "palhaco",
   "pau",
+  "penis",
+  "pervertido",
+  "pica",
   "piranha",
   "piroca",
+  "piss",
   "porra",
+  "prick",
+  "punheta",
   "puta",
   "putaria",
   "puto",
+  "retardado",
   "rola",
+  "sacanagem",
+  "safada",
+  "safado",
+  "shit",
+  "sonofabitch",
+  "slut",
+  "stupid",
+  "sucker",
+  "tesao",
+  "vaca",
   "vagabunda",
-  "viado",
-  "gay"
-]);
+  "vagabundo",
+  "vsf",
+  "wanker",
+  "wtf",
+  "whore"
+];
+
+const exactMatchTerms = new Set(bannedTerms);
+const embeddedMatchTerms = [...new Set(bannedTerms)]
+  .filter((term) => term.length >= 4)
+  .sort((firstTerm, secondTerm) => secondTerm.length - firstTerm.length);
 
 function normalizeText(value) {
   return value
@@ -40,6 +106,10 @@ function tokenizeText(value) {
     .filter(Boolean);
 }
 
+function compactText(value) {
+  return normalizeText(value).replace(/[^a-z0-9]+/g, "");
+}
+
 function findBannedTerm(value) {
   if (typeof value !== "string") {
     return null;
@@ -48,8 +118,16 @@ function findBannedTerm(value) {
   const tokens = tokenizeText(value);
 
   for (const token of tokens) {
-    if (bannedTerms.has(token)) {
+    if (exactMatchTerms.has(token)) {
       return token;
+    }
+  }
+
+  const compactValue = compactText(value);
+
+  for (const term of embeddedMatchTerms) {
+    if (compactValue.includes(term)) {
+      return term;
     }
   }
 
